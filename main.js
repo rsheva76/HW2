e12a20c21df2b911a27cdef1feb4f666a15e5cbd
Ruslan <99551828+rsheva76@users.noreply.github.com>
2022-02-19 21:46:09 +0200

    home work 2

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict'
+//first number
+let firstNum;
+do {
+   firstNum = +prompt('Введіть число N:', '')
+} while (!Number.isInteger(firstNum) || firstNum == NaN)
+console.log('Первое число: ', firstNum)
+
+//second number
+let secondNum;
+do {
+   secondNum = +prompt('Введіть число M:', '')
+} while (!Number.isInteger(secondNum) || secondNum == NaN)
+console.log('Второе число: ', secondNum)
+
+//boolean
+const doubleNumbers = confirm("Пропускати парні числа?");
+console.log('Парне число чи ні: ', doubleNumbers)
+
+//sum of numbers - paired or unpaired
+let sum = 0
+for (let i = firstNum; i <= secondNum; i++) {
+   if (doubleNumbers && i % 2 === 0) continue
+   sum += i
+}
+
+console.log('Сума парних чи непарних чисел: ', sum)
+
+// outputting data to a page
+document.getElementById("first").innerHTML = firstNum;
+document.getElementById("second").innerHTML = secondNum;
+document.getElementById("boolean").innerHTML = doubleNumbers;
+document.getElementById("sum").innerHTML = sum;
+
